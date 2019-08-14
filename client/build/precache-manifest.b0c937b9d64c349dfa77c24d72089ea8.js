f852f466b6f889e7f3a4c2518c11e4b310404664
Akshat Vasudev <akshat.vasudev@wework.com>
2019-08-14 12:31:04 -0400

    build

---
+++ client/build/precache-manifest.b0c937b9d64c349dfa77c24d72089ea8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3555c4f4b940da01cf602f185b7718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e78f7b5e18c122a68f7a",
+    "url": "/static/css/main.47b44e8a.chunk.css"
+  },
+  {
+    "revision": "0b9e8c6d44b742661042",
+    "url": "/static/js/2.7ce05fe5.chunk.js"
+  },
+  {
+    "revision": "e78f7b5e18c122a68f7a",
+    "url": "/static/js/main.ba60934a.chunk.js"
+  },
+  {
+    "revision": "aac10b1cbb2aec953c81",
+    "url": "/static/js/runtime~main.bdc68d36.js"
+  }
+]);
